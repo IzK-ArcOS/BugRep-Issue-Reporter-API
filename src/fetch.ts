7eef194817a0aa887902af059c7581e54ec17166
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-09-05 21:41:05 +0200

    Delete fetch.ts

--- src/fetch.ts
+++
@@ -1,6 +0,0 @@
-import { RequestInfo, RequestInit } from "node-fetch";
-
-const fetch = (url: RequestInfo, init?: RequestInit) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(url, init));
-
-export default fetch;
